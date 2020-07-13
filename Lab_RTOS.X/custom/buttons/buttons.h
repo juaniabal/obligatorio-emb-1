bf4d39dda58ff6ffb6d232cf030ac699f5760372
Martinengelhardt <37815975+Martinengelhardt@users.noreply.github.com>
2020-07-13 20:11:16 -0300

    subo buttons

---
+++ Lab_RTOS.X/custom/buttons/buttons.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/** Descriptive File Name
+
+  @Company
+    Company Name
+
+  @File Name
+    filename.h
+
+  @Summary
+    Brief description of the file.
+
+  @Description
+    Describe the purpose of this file.
+ */
+/* ************************************************************************** */
+
+#ifndef buttons    /* Guard against multiple inclusion */
+#define buttons
+
+
+/* ************************************************************************** */
+/* ************************************************************************** */
+/* Section: Included Files                                                    */
+/* ************************************************************************** */
+/* ************************************************************************** */
+
+/* This section lists the other files that are included in this file.
+ */
+
+/* TODO:  Include other files here if needed. */
+
+
+
+#endif /* _EXAMPLE_FILE_NAME_H */
+
+/* *****************************************************************************
+ End of File
+ */
