76994bf1461de4e570b3b0df4548c61ea0c2a3ca
Juan <juaniabal7@gmail.com>
2020-07-15 16:39:14 -0300

    Logs

---
+++ Lab_RTOS.X/custom/Logs/logs.c
@@ -0,0 +1,40 @@
+#include "logs.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "../../mcc_generated_files/adc1.h"
+
+
+void WriteLogs(){//PROTEGER CON SEMAFORO?
+    uint8_t i = 0;
+    char logWriter[45];
+    char tempLog[8];
+    char registro[4];
+    while(i<logPosition){
+         memset(logWriter, 0, sizeof(logWriter) );
+                                    
+        strcpy(logWriter,"Registro ");
+        if(i==0){
+            strcpy(registro,"0");
+        }else{
+            sprintf(registro, "%d", i);
+        }
+        strcat(logWriter,registro);
+        strcat(logWriter,"\n Temp: ");
+                                    
+        sprintf(tempLog, "%d", logsEvents[i].temp);
+                                    
+        strcat(logWriter,tempLog);
+        USB_sendS(logWriter);
+                                    
+        i++;
+    }
+    i=0;
+}
+void AddLog(logger event){
+    if(logPosition<=LOGS){        
+        logsEvents[logPosition] = event; 
+        logPosition++;
+    }
+}
---
+++ Lab_RTOS.X/custom/Logs/logs.h
@@ -0,0 +1,27 @@
+
+#ifndef LOGS_H    
+#define LOGS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <time.h>
+#include <string.h>
+#include "../GPS/GPS.h"
+#include <inttypes.h>
+
+#define LOGS 5
+
+typedef struct {//id registro es la posicion
+    time_t time;
+    GPSPosition_t ubicacion;
+    uint16_t temp;//
+} logger;
+//static uint8_t ejemplologsEvents[3];
+static logger logsEvents[LOGS];
+static uint8_t logPosition =0;
+
+
+    
+void WriteLogs();
+void AddLog(logger event);
+#endif
